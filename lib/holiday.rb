9bdc3c67865dd1ee3d1e3b03ac0ce2764609583a
Brent Bauman <brentbauman86@gmail.com>
2018-04-25 14:02:17 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ holiday_hash[:winter].values.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
-holiday_hash.each do |season, holiday|
+holiday_hash.each do |season|
   puts "#{season.capitalize}:"
   holiday.each do |holiday_name, supplies|
       puts "  #{holiday_name.to_s.split("_").collect
